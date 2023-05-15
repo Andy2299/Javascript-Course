d58deac4b7f5ba002c5776a4918e11beb9043f9e
Andy2299 <90882478+Andy2299@users.noreply.github.com>
2023-05-14 20:54:24 -0600

    Actividad 1

--- activity/1_session/aboutme.js
+++ activity/1_session/aboutme.js
@@ -1,4 +1,11 @@
-console.log("hello world");
+// Definición de las variables
+const nombre = "Jorge Andrey Rodríguez Piedra";
+const profesion = "diseñar y desarrollar aplicaciónes web";
+const puestoDeseado = "full stack developer";
 
+// Creación de la cadena utilizando Template Literals
+const presentacion = `Hola, me llamo ${nombre} y me dedico a ${profesion}. Estoy cursando este Máster porque me gustaría trabajar como ${puestoDeseado}`;
 
-console.log("laater");
+// Mostrar por pantalla la variable presentacion
+console.log("Ejercicio 1");
+console.log(presentacion);
--- activity/1_session/index.js
+++ activity/1_session/index.js
@@ -1,14 +1,17 @@
 // Ejercicio 1
 import "./aboutme.js";
-console.log("Ejercicio 1");
+
+
 
 // Ejercicio 2
 
 // Ejercicio 3
 
 // Ejercicio 4
+import "./ejercicio4.js";
 
 // Ejercicio 5
 
 // Ejercicio 6
+import "./ejerciocio6.js"
 
